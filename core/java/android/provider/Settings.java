af9661b307cc3bf2ead9b66f395aeb0881c20ea1
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-27 17:05:57 +0700

    SystemUI: Add support for GameSpace
    This is an addon for GameSpace for broadcasting about game start/stop.
    Additionally, it also has special option for suppressing fullscreen
    intent like incoming call.
    
    This also squash commits:
    - SystemUI: Improve GameSpace lifecycle and broadcast handling
    - GameSpaceManager: Handle various case against "locking screen" scenario
    - GameSpace: Fix issue with multiple users [neobuddy89]
    - SystemUI: Update gamespace for A13 [Genkzsz11]
    - GameSpaceManager: restrict broadcast to MANAGE_GAME_MODE holders only
    - GameSpaceManager: Update TaskStack listener usage
    - GameSpaceManager: Updated for A14 QPR1 [neobuddy89]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6427,14 +6427,18 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
 
         /**
+         * GameSpace: List of added games by user
          * @hide
          */
-        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+        @Readable
+        public static final String GAMESPACE_GAME_LIST = "gamespace_game_list";
 
         /**
+         * GameSpace: Whether fullscreen intent will be suppressed while in game session
          * @hide
          */
-        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+        @Readable
+        public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
