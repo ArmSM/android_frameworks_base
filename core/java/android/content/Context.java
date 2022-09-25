7a72653b3db23e8141780e602b4b0e29460593a6
cjh1249131356 <cjh1249131356@gmail.com>
2022-09-25 14:50:31 +0800

    base: Add mock oplus LinearmotorVibratorService
    * To get haptic feedback in OnePlus / Oplus Camera.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [@neobuddy89: Remove unnecessary bits.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -6735,6 +6735,14 @@ public abstract class Context {
      * @hide
      */
     public static final String SUPERVISION_SERVICE = "supervision";
+     * Use with {@link #getSystemService} to retrieve a
+     * {@link com.oplus.os.LinearmotorVibrator} for accessing linear motor vibrator state.
+     *
+     * @hide
+     * @see #getSystemService
+     * @see com.oplus.os.LinearmotorVibrator
+     */
+    public static final String LINEARMOTOR_VIBRATOR_SERVICE = "linearmotor";
 
     /**
      * Determine whether the given permission is allowed for a particular
